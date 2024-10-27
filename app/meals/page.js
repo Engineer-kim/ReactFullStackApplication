16f88922482a0b4c577feeca52f40cf62791a1f7
Engineer-kim <ddang0103@naver.com>
2024-10-27 17:46:09 +0900

    Feature: 각 nav 메뉴들에 대한 상세 페이지 화면단 구성 작성 완료

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from 'next/link'
+import classes from '../meals/page.module.css'
+import MealsGrid from '../components/meals/meals-grid'
+
 export default function MealsPage(){
-    return <h1>Meals Page</h1>
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{''}
+                    <span className={classes.highlight}>by You</span>
+                </h1>
+                <p>Choose U r favorite recipe and cook it yourself. It is easy and fun!</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share Your favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]}/>
+            </main>
+        </>
+    )
 }
