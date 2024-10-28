f0d36297fd80370dc5851490b8dc84899f0c26c8
Engineer-kim <ddang0103@naver.com>
2024-10-28 13:32:35 +0900

    Feature: SqlLite3 를 이용하여 더미데이터를 활용하여 상품 리스트 리턴

--- app/components/meals/meals-grid.js
+++ app/components/meals/meals-grid.js
@@ -3,10 +3,15 @@ import classes from '../../components/meals/meals-grid.module.css'
 import MealItem from './meal-item'
 
 export default function MealsGrid({meals}){
+    console.log("meals1222222222222" ,meals)
     return (
         <ul className={classes.meals}>
-            {meals.map(meal=>{<li key={meal.id}>
-                <MealItem  {...meal}/>
-        </li>})}</ul>
+            {meals.map((meal) => (
+                <li key={meal.id}>
+                    {console.log(meal)}
+                    <MealItem {...meal} />
+                </li>
+            ))}
+    </ul>
     )
 }
