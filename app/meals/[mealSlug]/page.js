1238cfc6aed124cff44239944e2d01a74ad69023
Engineer-kim <ddang0103@naver.com>
2024-10-28 15:05:24 +0900

    Feature: 각 제품들에 대한 상세페이지 컴포넌트 작성

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,29 @@
-export default function MealsDetailPage(){
-    return <h1>Meals Detail Page</h1>
+import { getMeal } from '@/lib/meals'
+import clasess from '../[mealSlug]/page.module.css'
+import Image from 'next/image'
+
+export default function MealsDetailPage({ params }) {
+    const meal = getMeal(params.mealSlug)
+    meal.instructions = meal.instructions.replace(/\n/g  ,'<br />')
+    return <>
+        <header className={clasess.header}>
+            <div className={clasess.image}>
+                <Image src={meal.image} alt={meal.title} fill />
+            </div>
+            <div className={clasess.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={clasess.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+                <p className={clasess.summary}>
+                    {meal.summary}
+                </p>
+            </div>
+        </header>
+        <main>
+            <p className={clasess.instructions} dangerouslySetInnerHTML={{
+                __html: meal.instructions,
+            }}></p>
+        </main>
+    </>
 }
