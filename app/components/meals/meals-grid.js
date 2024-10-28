27d16c16aeb7f2293188ba99c7b4a5ca58d7b0ea
Engineer-kim <ddang0103@naver.com>
2024-10-28 15:29:15 +0900

    Fix: 불필요한 로그 출력 삭제

--- app/components/meals/meals-grid.js
+++ app/components/meals/meals-grid.js
@@ -3,12 +3,10 @@ import classes from '../../components/meals/meals-grid.module.css'
 import MealItem from './meal-item'
 
 export default function MealsGrid({meals}){
-    console.log("meals1222222222222" ,meals)
     return (
         <ul className={classes.meals}>
             {meals.map((meal) => (
                 <li key={meal.id}>
-                    {console.log(meal)}
                     <MealItem {...meal} />
                 </li>
             ))}
